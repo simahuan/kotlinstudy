778e9f8eda16b4940e5770bab08fa065f0c27b2d
lx <lxfbi@qq.com>
2019-08-01 10:28:51 +0800

    [added] basicData test

---
+++ src/main/kotlin/com/pisen/less01/BasicData.kt
@@ -0,0 +1,16 @@
+package com.pisen.less01
+
+
+fun boolean (){
+    val aBoolean:Boolean = false
+
+    val bBoolean:Boolean = true
+
+    println(aBoolean)
+    println(bBoolean)
+}
+
+
+fun main() {
+    boolean()
+}
