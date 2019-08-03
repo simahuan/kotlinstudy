48c8631baac410da4c4f9ffc5bb3379ff40d7ea2
simahuan <455215547@qq.com>
2019-08-03 10:23:13 +0800

    [add] 函数引用的三种方式

--- src/main/kotlin/com/pisen/hof/hof01.kt
+++ src/main/kotlin/com/pisen/hof/hof01.kt
@@ -1,14 +1,25 @@
 package com.pisen.hof
 
+import javafx.beans.binding.Bindings.isNotEmpty
+
 fun main(args:Array<String>) {
 
 //    args.forEach { ::println }
-    args.forEach(::println)
+    args.forEach(::println)  //包级函数引用
 
-    val helloWorld = Hello::world
+    val helloWorld = Hello::world  //成员函数引用
 
     args.filter(String::isNotEmpty)
+    args.filter (::isNotEmpty)
+    val pdfPrinter = PdfPrint()
+    args.forEach (pdfPrinter::println) //receiver :: println  形式的函数引用
 
+}
+
+class PdfPrint(){
+    fun println(any:Any?){
+        kotlin.io.println(any)
+    }
 }
 
 class Hello{
